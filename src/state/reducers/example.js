ab6ce2f1fd1a1ea39fe45e3cfce99a2c6587a697
Leigh Quince <leigh.quince@domo-safety.com>
2020-08-31 14:01:21 +0200

    formatting

--- src/state/reducers/example.js
+++ src/state/reducers/example.js
@@ -1,15 +1,15 @@
 export const initialState = {
-	items: {},
-	loadError: false,
-	isLoading: false,
-	errorMessage: null,
+  items: {},
+  loadError: false,
+  isLoading: false,
+  errorMessage: null,
 };
 
 const reducer = (state = initialState, action) => {
-	switch (action.type) {
-		default:
-			return state;
-	}
+  switch (action.type) {
+    default:
+      return state;
+  }
 };
 
 export default reducer;
--- src/state/sagas/index.js
+++ src/state/sagas/index.js
@@ -2,5 +2,5 @@
 import { all } from "redux-saga/effects";
 
 export default function* rootSaga() {
-	yield all([]);
+  yield all([]);
 }
